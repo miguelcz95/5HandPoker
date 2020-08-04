15afb14853180ec23618131e2f2f729bddd1fb46
miguelcz95 <developermiguel95@gmail.com>
2020-08-03 22:09:21 -0500

    Initial structure

--- src/co/com/cardgame/core/Game.java
+++ src/co/com/cardgame/core/Game.java
@@ -2,17 +2,10 @@ package co.com.cardgame.core;
 
 import co.com.cardgame.model.Player;
 
-import java.util.logging.Logger;
-
 public class Game {
 
-
-    final Logger LOGGER = Logger.getLogger(this.getClass().getName());
-
     public void runGame() {
-        PokerHands pokerHands = new PokerHands();
         SetUp setUp = new SetUp();
-        Mechanics mechanics = new Mechanics();
 
         Player player = new Player();
         player.setHand(setUp.getHand());
@@ -21,17 +14,6 @@ public class Game {
         Player robot = new Player();
         robot.setHand(setUp.getHand());
         System.out.println("Robot Hand: " + robot.getHand().toString());
-
-        int playerScore = mechanics.evaluateHands(player.getHand());
-        int robotScore = mechanics.evaluateHands(robot.getHand());
-
-        int playerHighCard = pokerHands.isHighCard(player.getHand());
-        int robotHighCard = pokerHands.isHighCard(robot.getHand());
-
-        System.out.println("player High Card: " + playerHighCard);
-        System.out.println("robot High Card: " + robotHighCard);
-
-        System.out.println(mechanics.whoIsTheWinner(playerScore, robotScore, playerHighCard, robotHighCard));
 
     }
 
